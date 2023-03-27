24bb175ca9d721069c6fa85066e62c9dc69e4037
MaciejSorko <82665647+MaciejSorko@users.noreply.github.com>
2023-03-27 11:03:57 +0000

    Deploying to gh-pages from @ MaciejSorko/goit-js-hw-09@6f35921cd88202b1ca2538156ca1b1f68cd2b104 🚀

---
+++ 01-color-switcher.33a8110a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0,e.addEventListener("click",(()=>{timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(timerId),console.log(`Interval with id ${timerId} has stopped!`),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.33a8110a.js.map

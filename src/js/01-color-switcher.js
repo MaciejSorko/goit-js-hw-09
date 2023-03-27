b0b28d2845844e7f897067e3fcdd0d7cf31fdaf6
ElchaX <maciej.sorko@gmail.com>
2023-03-27 15:31:46 +0200

    some fixes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,12 +1,13 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 stopBtn.disabled = true;
+let timerId = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 startBtn.addEventListener('click', () => {
-  const timerId = setInterval(() => {
+  timerId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
   startBtn.disabled = true;

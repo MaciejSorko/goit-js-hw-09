22a38a35ee379064dd3837d0eac440dce693532e
ElchaX <maciej.sorko@gmail.com>
2023-03-29 21:52:07 +0200

    task 3 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,6 +8,14 @@ const dataDays = document.querySelector('[data-days]');
 const dataHours = document.querySelector('[data-hours]');
 const dataMin = document.querySelector('[data-minutes]');
 const dataSec = document.querySelector('[data-seconds]');
+
+const timerItem = document.querySelector('.timer');
+timerItem.style.display = 'flex';
+timerItem.style.justifyContent = 'space-evenly';
+timerItem.style.width = '25%';
+timerItem.style.marginLeft = '24px';
+timerItem.style.fontSize = '24px';
+
 const addLeadingZero = value => {
   return value.toString().padStart(2, '0');
 };
@@ -34,41 +42,36 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-      const dateStatic = new Date();
-      if (selectedDates[0].getTime() > dateStatic.getTime()) {
-        startBtn.disabled = false;
-        startBtn.addEventListener('click', () => {
-          startBtn.disabled = true;
-          document.querySelector('input#datetime-picker').disabled = true;
-          countdown = setInterval(() => {
-            let date = new Date();
-            dataDays.textContent = addLeadingZero(
-              convertMs(selectedDates[0] - date.getTime()).days
-            );
-            dataHours.textContent = addLeadingZero(
-              convertMs(selectedDates[0] - date.getTime()).hours
-            );
-            dataMin.textContent = addLeadingZero(
-              convertMs(selectedDates[0] - date.getTime()).minutes
-            );
-            dataSec.textContent = addLeadingZero(
-              convertMs(selectedDates[0] - date.getTime()).seconds
-            );
-            if ((selectedDates[0].getTime() - date.getTime())  < 1000) {
-              clearInterval(countdown);
-            }
-          }, 1000);
-         
-        });
-      } else {
-        window.alert('Please choose a date in the future');
+  onClose(selectedDates) {
+    const dateStatic = new Date();
+    if (selectedDates[0].getTime() > dateStatic.getTime()) {
+      startBtn.disabled = false;
+      startBtn.addEventListener('click', () => {
         startBtn.disabled = true;
-      }
+        document.querySelector('input#datetime-picker').disabled = true;
+        countdown = setInterval(() => {
+          let date = new Date();
+          dataDays.textContent = addLeadingZero(
+            convertMs(selectedDates[0] - date.getTime()).days
+          );
+          dataHours.textContent = addLeadingZero(
+            convertMs(selectedDates[0] - date.getTime()).hours
+          );
+          dataMin.textContent = addLeadingZero(
+            convertMs(selectedDates[0] - date.getTime()).minutes
+          );
+          dataSec.textContent = addLeadingZero(
+            convertMs(selectedDates[0] - date.getTime()).seconds
+          );
+          if (selectedDates[0].getTime() - date.getTime() < 1000) {
+            clearInterval(countdown);
+          }
+        }, 1000);
+      });
+    } else {
+      window.alert('Please choose a date in the future');
+      startBtn.disabled = true;
+    }
   },
 };
 flatpickr('input#datetime-picker', options);
-
-
-
-
